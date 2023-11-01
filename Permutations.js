961999876d532a971738719070dda72589588b7c
Ice Nantawat <nuntawat_168@outlook.com>
2023-11-01 16:20:44 +0700

    fix: permatations test

--- Permutations.js
+++ Permutations.js
@@ -14,43 +14,53 @@ String.prototype.replaceAt = function (index, replacement) {
   );
 };
 
-function permutations(strings) {
-  const result = [strings];
-  let lastShuffle = strings;
-  let shuffleIndex = 0;
-  for (let i = 1; i < factorialize(strings.length); i++) {
-    let tmpLastShuffle = lastShuffle;
-    tmpLastShuffle = tmpLastShuffle.replaceAt(
-      shuffleIndex,
-      lastShuffle[shuffleIndex + 1]
-    );
-    tmpLastShuffle = tmpLastShuffle.replaceAt(
-      shuffleIndex + 1,
-      lastShuffle[shuffleIndex]
-    );
+function swap(strings, index1, index2) {
+  let tmpStrings = strings;
+  tmpStrings = tmpStrings.replaceAt(index1, strings[index2]);
+  tmpStrings = tmpStrings.replaceAt(index2, strings[index1]);
+  return tmpStrings;
+}
 
-    // console.log(tmpLastShuffle);
-    result.push(tmpLastShuffle);
-    lastShuffle = tmpLastShuffle;
-    if (shuffleIndex === strings.length - 2) {
-      shuffleIndex = 0;
-    } else {
-      shuffleIndex++;
+// function recusPermutaions(strings, startIndex, endIndex) {
+//   if (startIndex === endIndex) {
+//     console.log(strings);
+//     return strings;
+//   }
+//   for (let i = startIndex; i <= endIndex; i++) {
+//     const swapStrings = swap(strings, startIndex, i);
+//     recusPermutaions(swapStrings, startIndex + 1, endIndex);
+//   }
+// }
+
+function permutations(strings) {
+  const result = [];
+
+  //   result.push(recusPermutaions(strings, 0, strings.length - 1));
+  function recusPermutaions(strings, startIndex, endIndex) {
+    if (startIndex === endIndex) {
+      // console.log(strings);
+      //   return strings;
+      result.push(strings);
+    }
+    for (let i = startIndex; i <= endIndex; i++) {
+      const swapStrings = swap(strings, startIndex, i);
+      recusPermutaions(swapStrings, startIndex + 1, endIndex);
     }
   }
-  console.log(result);
+
+  recusPermutaions(strings, 0, strings.length - 1);
+
   return result.filter((string, index) => result.lastIndexOf(string) === index);
-  return result;
 }
 
 console.log(permutations("a"));
-// console.log(permutations("ab"));
-// console.log(permutations("abc"));
+console.log(permutations("ab"));
+console.log(permutations("abc"));
 console.log(permutations("aabb"));
 console.log(permutations("abba"));
 console.log(permutations("bbaa"));
 console.log(permutations("baba"));
 console.log(permutations("baab"));
 console.log(permutations("abab"));
-
 console.log(permutations("1234"));
+console.log(permutations("12345"));
